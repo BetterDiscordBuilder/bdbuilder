8d1f85a08d8a34181b0dba9c79b808ca7072873a
Strencher <46447572+Strencher@users.noreply.github.com>
2021-06-09 23:50:33 +0200

    Add preview support.

--- core/classes/readme.js
+++ core/classes/readme.js
@@ -1,3 +1,5 @@
+import fs from "fs-extra";
+import path from "path";
 import Utils from "../utils.js";
 
 export const string = `
@@ -6,7 +8,7 @@ export const string = `
 > {{description}}
 <hr/>
 {{contributors}}
-<br/>
+{{previews}}
 <span>Made with <img src="https://discord.com/assets/0483f2b648dcc986d01385062052ae1c.svg" width="15" /> by <a href="https://github.com/Kyza/bdbuilder">BDBuilder</a></span>
 `.trim(); 
 
@@ -26,6 +28,16 @@ export const contributorsTable = `
 </table>
 `;
 
+export const preview = `
+## {{name}}
+![image]({{src}})
+`;
+
+export const previewsPage = `
+# Previews
+{{previews}}
+`;
+
 export default class Readme {
     constructor(config) {
         this.config = config;
@@ -33,14 +45,32 @@ export default class Readme {
 
     get contributors() {
         const contributors = this.config.build.release.contributors;
-        if (!Array.isArray(contributors)) return "";
+        if (!Array.isArray(contributors) || !previews.length) return "";
         
         return Utils.format(contributorsTable, {
-            contributors: contributors.map(e => {
-                console.log(e);
-                return Utils.format(contributor, e);
-            }).join("\n")
-        });
+            contributors: contributors.map(e => Utils.format(contributor, e)).join("\n")
+        }) + "<br/>";
+    }
+
+    get previews() {
+        const previews = this.config.build.release.previews;
+
+        if (!Array.isArray(previews) || !previews.length) return "";
+
+        return Utils.format(previewsPage, {
+            previews: previews.map(e => {
+                return Utils.format(preview, {
+                    name: e.name,
+                    src: this.makeSrcLink(e)
+                })
+            }).join("<br/>\n")
+        }) + "<br/>"
+    }
+
+    makeSrcLink(img) {
+        if (this.config.build.release.public) {
+            return Utils.resolveRawGithub(this.config.info.name, `src/${img.src}`);
+        } else return `data:image/${path.extname(img.src).slice(1)};base64,${fs.readFileSync(path.resolve(Utils.getPath(), img.src)).toString("base64")}`;
     }
 
     toString() {
@@ -50,6 +80,10 @@ export default class Readme {
 
         output = Utils.format(output, {
             contributors: this.contributors
+        });
+
+        output = Utils.format(output, {
+            previews: this.previews
         });
 
         return output;
