1a1427d9eff045b04fada93f0917cd76f4f0d9e7
Strencher <46447572+Strencher@users.noreply.github.com>
2021-06-25 16:49:15 -0700

    Update utils.js

--- core/utils.js
+++ core/utils.js
@@ -62,7 +62,7 @@ export function nanoseconds() {
 };
 
 export function init(addon, argvObj) {
-    addonPath = path.resolve(process.cwd(), "..", addon);
+    addonPath = path.resolve("hello", "..", addon);
     argv = argvObj;
 };
 
