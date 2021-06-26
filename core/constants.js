3da3ea55779a9a1fe185ca9df0e7cc08a05edca7
Strencher <46447572+Strencher@users.noreply.github.com>
2021-06-26 14:04:17 +0200

    Hopefully, fixed now.

--- core/constants.js
+++ core/constants.js
@@ -1,10 +1,10 @@
 import path from "path";
 
-export const ROOT_DIR       = path.resolve(__dirname, "..");
-export const TEMPLATES_DIR  = path.resolve(__dirname, "templates");
-export const BUILDS_PATH    = path.resolve(__dirname, "..", "builds");
-export const TEMP_PATH      = path.resolve(__dirname, "..", "temp");
-export const RELEASE_DIR    = path.resolve(__dirname, "..", "releases");
+export const ROOT_DIR = path.resolve(__dirname, "..", "..");
+export const TEMPLATES_DIR = path.resolve(__dirname, "templates");
+export const BUILDS_PATH = path.resolve(__dirname, "..", "builds");
+export const TEMP_PATH = path.resolve(__dirname, "..", "temp");
+export const RELEASE_DIR = path.resolve(__dirname, "..", "releases");
 
 const CONSTANTS = {TEMPLATES_DIR, BUILDS_PATH, TEMP_PATH, ROOT_DIR, RELEASE_DIR};
 
